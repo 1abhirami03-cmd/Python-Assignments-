3cc204af5c2b8879f46abbe2db724c04670820b7
1abhirami03-cmd <1abhirami03@gmail.com>
2026-09-05 07:28:14 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,33 @@
+string1 ="Hello "
+string2 ="Abhirami"
+result = string1+string2
+print(result)
+
+string3 =", Welcome to python programming"
+result= result+string3
+print(result)
+
+print(result[0])
+print(result[-1])
+print(result[0:5])
+print(result[-11::])
+print(result[::-1])
+print(result[27:33])
+
+
+strM = "Python beginer tutorial"
+print(strM.upper())
+print(strM.lower())
+print(strM.capitalize())
+print(strM.count("t"))
+print(strM.replace("Python","Machine learning"))
+
+
+t1 =(10,20,30)
+t2 = (40,50,60)
+t_combine = t1+t2
+print(t_combine)
+print(t_combine *3)
+print(t_combine[2])
+print(t_combine[0:3])
+print(t_combine[-3:])
